35af224f6f360333c8482a08895d0dd9eda81d39
Vlad Gheorghita <vladgh112@gmail.com>
2021-06-17 23:20:26 +0300

    Exercise details style

--- app/views/info/exerciseDetails.php
+++ app/views/info/exerciseDetails.php
@@ -6,26 +6,25 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title><?= APPNAME ?> - exercise details</title>
+	<title><?= APPNAME ?> - <?= $data->name ?></title>
 	<link rel="stylesheet" href='<?= URLROOT ?>/public/src/css/style.css'>
 	<link rel="stylesheet" href='<?= URLROOT ?>/public/src/css/header.css'>
 	<link rel="stylesheet" href='<?= URLROOT ?>/public/src/css/exerciseDetails.css'>
-
 </head>
 
 <body>
 	<?php include_once APPROOT . '/views/includes/loggedInHeader.php'; ?>
-	<?php
-	echo "<a href='" . $data->link . "'><img class='exercise--container' src='\\WoG\\app\\Img\\" . $data->photo . "'></a>";
-	echo "<br>";
-	echo "Focus: " . $data->focus;
-	echo "<br>";
-	echo "Intended: " . $data->intended;
-	echo "<br>";
-	echo "Descriere: " . $data->description;
-	echo "<br>";
-	echo '<iframe width="350" height="200" src="'. $data->link . '" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
-		?>
+
+	<section class="exercise--container">
+		<a class="image--container" href="<?=$data->link ?>"><img class="exercise-image" src="<?= URLROOT ?>/app/Img/<?= $data->photo ?>"></a>
+		<div class="text--container">
+			<h2><?= $data->name ?></h2>
+			<p>Focus: <?= $data->focus ?></p>
+			<p>Intended: <?= $data->intended ?></p>
+			<p>Description: <?= $data->description ?></p>
+		</div>
+		<iframe src="<?= $data->link ?>" frameborder="0" width="350" height="200" title="YouTube video player" clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+	</section>
 </body>
 
 </html>
